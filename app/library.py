ccbfe592380a6b2134b4803fa22c1db72c04444d
annstephen <ann.stephen.88@gmail.com>
2014-04-15 18:08:53 -0500

    merging library

--- app/library.py
+++ app/library.py
@@ -9,14 +9,14 @@ class TweepyAPIs (object):
 
   def __init__ (self):
     # Put your twitter credentials here.
-    consumer_key = ""
-    consumer_secret = ""
-    access_key = ""
-    access_secret = ""
+    consumer_key = "sLA3Hp02Fki2vUWufjNAi1kpS"
+    consumer_secret = "hZImLluULkLwPXjunalcVkKT49AuQkxcRrAH4YmdxRfGHJrHLy"
+    access_key = "1243936562-e9Q0mpNRpoMS2ksYwE0dNtrMsCYt6ZR4z70lYBy"
+    access_secret = "recB79Pd2ZixUoPzXufkkRyeuXbRybcM7gycLMGK3cEds"
   
     if not (consumer_key and consumer_secret and access_key and access_secret):
-      print "Please enter all of your credentials."
-      sys.exit()
+        print "Please enter all of your credentials."
+        sys.exit()
   
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
