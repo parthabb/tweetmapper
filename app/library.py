4810e55d49433f893d226dedc34fee96ee1ba9bb
Partha Baruah <parthabb@gmail.com>
2014-04-15 17:55:30 -0500

    remove ids

--- app/library.py
+++ app/library.py
@@ -8,10 +8,10 @@ class TweepyAPIs (object):
   """Base class for all needed tweepy APIs"""
   def __init__ (self):
     # Put your twitter credentials here.
-    consumer_key = "W48HuKv5qcEl9bryHwAjA"
-    consumer_secret = "vf4M7dxlXQRuwdM1dASB0sg0ZkxnlMSWLWNc9B4"
-    access_key = "596178706-6SvITfh1xSdjUkIv5cvCQuHr8b5aD8sX6WxAl6AU"
-    access_secret = "IS0Wcplvg6WUwGVCFC53tdszsjoX850orNEmFSVDeWj6O"
+    consumer_key = ""
+    consumer_secret = ""
+    access_key = ""
+    access_secret = ""
 
     if not (consumer_key and consumer_secret and access_key and access_secret):
       print "Please enter all of your credentials."
