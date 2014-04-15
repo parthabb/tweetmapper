8f4976b2f1bb5d0332e1bd7ec7ed3333c7d93053
Partha Baruah <parthabb@gmail.com>
2014-04-15 17:01:29 -0500

    put tweepy into a class

--- app/library.py
+++ app/library.py
@@ -4,17 +4,20 @@ import tweepy
 import sys
 
 
-def get_tweepy_instance (self):
-  # Put your twitter credentials here.
-  consumer_key = ""
-  consumer_secret = ""
-  access_key = ""
-  access_secret = ""
+class TweepyAPIs (object):
+  """Base class for all needed tweepy APIs"""
 
-  if not (consumer_key and consumer_secret and access_key and access_secret):
-    print "Please enter all of your credentials."
-    sys.exit()
-
-  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-  auth.set_access_token(access_key, access_secret)
-  return tweepy.API(auth, parser=tweepy.parsers.ModelParser())
+  def __init__ (self):
+    # Put your twitter credentials here.
+    consumer_key = ""
+    consumer_secret = ""
+    access_key = ""
+    access_secret = ""
+  
+    if not (consumer_key and consumer_secret and access_key and access_secret):
+      print "Please enter all of your credentials."
+      sys.exit()
+  
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    self._api = tweepy.API(auth, parser=tweepy.parsers.ModelParser())
