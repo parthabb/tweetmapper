cee9a8626c14a7f4047ebadefde13f33f6b65d22
Partha Baruah <parthabb@gmail.com>
2014-04-15 17:52:17 -0500

    get test code and comments

--- app/library.py
+++ app/library.py
@@ -6,30 +6,26 @@ import sys
 
 class TweepyAPIs (object):
   """Base class for all needed tweepy APIs"""
-  _trends_type = {
-      'current': 'trends_current',
-      'daily': 'trends_daily',
-      'weekly': 'trends_weekly',
-  }
-
   def __init__ (self):
     # Put your twitter credentials here.
-    consumer_key = ""
-    consumer_secret = ""
-    access_key = ""
-    access_secret = ""
-  
+    consumer_key = "W48HuKv5qcEl9bryHwAjA"
+    consumer_secret = "vf4M7dxlXQRuwdM1dASB0sg0ZkxnlMSWLWNc9B4"
+    access_key = "596178706-6SvITfh1xSdjUkIv5cvCQuHr8b5aD8sX6WxAl6AU"
+    access_secret = "IS0Wcplvg6WUwGVCFC53tdszsjoX850orNEmFSVDeWj6O"
+
     if not (consumer_key and consumer_secret and access_key and access_secret):
       print "Please enter all of your credentials."
       sys.exit()
-  
+
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
     self._api = tweepy.API(auth, parser=tweepy.parsers.ModelParser())
 
-  def GetTrends (self, trend_type='default'):
-    return getattr(self._api, TweepyAPIs._trends_type.get(trend_type,
-                                                          'trends'))()
+  def GetTrends (self):
+    """Retruns the available trends."""
+    return self._api.trends_available()
 
   def GetTrendsByLocation (self, woeid=23424977):
-    return self._api.trends_location(woeid)
+    """Get trends based on the locations WOEID. Default is US."""
+    return self._api.trends_place(woeid)
+
