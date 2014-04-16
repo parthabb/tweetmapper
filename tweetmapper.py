1c22783b5914bcbf8c76a43ebc60a3cbe1601c4b
Partha Baruah <parthabb@gmail.com>
2014-04-16 18:05:51 -0500

    deletions and modification to tweetmapper (added tfidf calculation)

--- tweetmapper.py
+++ tweetmapper.py
@@ -36,18 +36,18 @@ def get_all_file_names():
     for f in files:
       if f.endswith('.txt'):
         all_txt_files.append(os.path.join(root, f))
-  return all_txt_files 
+  return all_txt_files
 
 
 class TweetMapper (object):
   """Main tweetmapper application."""
   def __init__ (self):
     self.inverse_term_matrix = {}
+    self._total_docs = 0
 
   def read_tweet_from_file(self):
     filenames = get_all_file_names()
-
-    trends = [trend.lstrip('tweets/').rstrip('.txt') for trend in filenames]
+    self._total_docs = len(filenames)
 
     for trending_tweet_file in filenames:
 
@@ -57,13 +57,22 @@ class TweetMapper (object):
         tweets = pickle.load(f.read())
 
       for tweet in tweets:
-        self.construct_inverse_map(tweet)
+        self._construct_inverse_map(tweet)
+
+  def calculate_tfidf(self):
+    """Calculate the TF-IDF."""
+    for _, posting in self.inverse_term_matrix.iteritems():
+      idf = math.log10(float(self._total_docs)/float(len(posting.keys())))
+      for city, tf in posting.iteritems():
+        tfidf = float(1 + math.log10(tf)) * idf
+        posting[city] = tfidf
 
   def _case_fold (self, text):
     """Case the given word."""
     return text.lower()
 
-  def construct_inverse_map(self, tweet):
+  def _construct_inverse_map(self, tweet):
+    """Construct an inverse term dictionary."""
     tweet_city = classify_city_id(tweet['coordinates']['coordinates'])
 
     for token in re.findall('[a-zA-Z0-9]+', tweet['text']):
@@ -74,3 +83,6 @@ class TweetMapper (object):
         postings = self.inverse_term_matrix.get(token, {tweet_city: 0})
         postings[tweet_city] = postings.get(tweet_city, 0) + 1
         self.inverse_term_matrix[token] = postings
+
+  def contruct_training_data(self):
+    pass
